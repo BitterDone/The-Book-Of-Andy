6a23aa18bb6b469d75b42a09de906bf16204d601
Dan Bitter <bitter.daniel@gmail.com>
2025-09-02 14:52:00 -0400

    Clean and format 02-clean echos

--- scripts/02-clean-filenames.sh
+++ scripts/02-clean-filenames.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
-echo "cd transcripts"
+
+echo "-->          cd transcripts"
 cd transcripts
-echo "cd'd transcripts. make removeSpecialChars"
+
+echo "-->          cd'd transcripts. make removeSpecialChars"
 mkdir removeSpecialChars
-echo "made removeSpecialChars. move txt files"
+
+echo "-->          made removeSpecialChars. move txt files"
 mv *.txt ./removeSpecialChars/
-echo "moved txt files. cd removeSpecialChars"
+
+echo "-->          moved txt files. cd removeSpecialChars"
 cd removeSpecialChars/
 
-echo "cd'd removeSpecialChars. do for loop"
+echo "-->          cd'd removeSpecialChars. do for loop"
 for file in *;
 do
     mv "$file" ../$(echo "$file" | sed -e 's/[^A-Za-z0-9._-]/_/g'); done;
 
-echo "did for loop. cd out of removeSpecialChars"
+echo "-->          did for loop. cd out of removeSpecialChars"
 cd ..
-echo "cd'd out of removeSpecialChars. rm removeSpecialChars"
+
+echo "-->          cd'd out of removeSpecialChars. rm removeSpecialChars"
 rm -rf removeSpecialChars
-echo "rm'd removeSpecialChars. make cutLeadingPrefix"
+
+echo "-->          rm'd removeSpecialChars. make cutLeadingPrefix"
 mkdir cutLeadingPrefix
-echo "made cutLeadingPrefix. mv txt to cutLeadingPrefix"
+
+echo "-->          made cutLeadingPrefix. mv txt to cutLeadingPrefix"
 mv *.txt cutLeadingPrefix/
-echo "mv'd txt to cutLeadingPrefix. cd cutLeadingPrefix"
+
+echo "-->          mv'd txt to cutLeadingPrefix. cd cutLeadingPrefix"
 
 cd cutLeadingPrefix/
-echo "cd'd cutLeadingPrefix. do 2nd for"
+
+echo "-->          cd'd cutLeadingPrefix. do 2nd for"
 
 for file in *;
 do
@@ -35,12 +44,15 @@ do
     mv "$file" ../"$newname"; 
 done
 
+
 echo "did 2nd for. cd out of cutLeadingPrefix"
 cd ..
+
 echo "cd'd out of cutLeadingPrefix. rm cutLeadingPrefix"
 rm -rf cutLeadingPrefix
+
 echo "rm'd cutLeadingPrefix. done"
-ls -al transcripts
+ls -al
 
 
 
